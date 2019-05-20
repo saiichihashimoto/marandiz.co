3483214152f0d91328315d2b6735ebb02f9abfce
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-05-20 18:46:15 -0500

    embed google sheets data

--- src/index.web.js
+++ src/index.web.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { hydrate, render } from 'react-dom';
-import App from './App';
 import './index.css';
+import App from './components/App';
 
 const rootElement = document.getElementById('root');
+const googleSheets = JSON.parse(document.getElementById('googleSheets').innerHTML);
 const hydrateOrRender = rootElement.hasChildNodes() ? hydrate : render;
 
-hydrateOrRender(<App />, rootElement);
+hydrateOrRender(<App googleSheets={googleSheets} />, rootElement);
