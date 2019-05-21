4a46dfc03bd5cb9445dae85d1530be91bc146e75
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-05-20 19:06:00 -0500

    react-helmet

--- src/components/App/index.web.js
+++ src/components/App/index.web.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import logo from './logo.svg';
 import styles from './App.module.css';
 
 const App = () => (
 	<div className={styles.app}>
+		<Helmet>
+			<title>Pinterest Ad Examples</title>
+			<meta charset="utf-8" />
+			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+			<meta name="theme-color" content="#000000" />
+		</Helmet>
 		<header className={styles.appHeader}>
 			<img src={logo} className={styles.appLogo} alt="logo" />
 			<p>
