a61544bf01db4c44377afb740f79917b1c2ae6aa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-21 16:23:53 +0000

    Deploy pinterestadexamples.com to github.com/saiichihashimoto/pinterestadexamples.com.git:gh-pages

---
+++ precache-manifest.6912f9091109fe90c66ffed0fe7750a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58e5c06554d5e46812ae5ccf79c2abf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdd6f0f7c44fecf6ef19",
+    "url": "/static/css/main.320ae822.chunk.css"
+  },
+  {
+    "revision": "653cd1583e3516c1214e",
+    "url": "/static/js/2.380b20ca.chunk.js"
+  },
+  {
+    "revision": "cdd6f0f7c44fecf6ef19",
+    "url": "/static/js/main.97bfd900.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
