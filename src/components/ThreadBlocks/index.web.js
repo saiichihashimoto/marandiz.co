af096e6d4dc259ad608a3155709d764fdfeb2127
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-07-13 20:32:45 -0700

    tags filter threads

--- src/components/ThreadBlocks/index.web.js
+++ src/components/ThreadBlocks/index.web.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import StackGrid from 'react-stack-grid';
+import classnames from 'classnames';
+
 import styles from './ThreadBlocks.module.scss';
 
-const { rows: threads } = JSON.parse(document.querySelector('#googleSheets').innerHTML);
+const {
+	columns: {
+		tags: allTags = [],
+	},
+	rows: threads,
+} = JSON.parse(document.querySelector('#googleSheets').innerHTML);
 
 function ThreadBlocks() {
+	const [selectedTag, setTag] = useState(null);
+
+	const filteredThreads = selectedTag ?
+		threads.filter(({ tags = [] }) => tags.includes(selectedTag)) :
+		threads;
+
 	return (
-		<StackGrid
-			columnWidth={260}
-			gutterWidth={0}
-			gutterHeight={0}>
-			{threads.map((thread) => (
-				<div key={thread.id} className={styles.threadContainer}>
-					<a className={styles.thread} href={thread.url} target="_blank" rel="noopener noreferrer">
-						<pre>{JSON.stringify(thread, null, 4)}</pre>
-					</a>
-				</div>
+		<Fragment>
+			{allTags.map((tag) => (
+				<span
+					key={tag}
+					className={classnames(styles.tag, { [styles.selected]: selectedTag === tag })}
+					onClick={() => setTag(selectedTag === tag ? null : tag)}>
+					{tag}
+				</span>
 			))}
-		</StackGrid>
+			<StackGrid
+				columnWidth={260}
+				gutterWidth={0}
+				gutterHeight={0}>
+				{filteredThreads.map((thread) => (
+					<div key={thread.id} className={styles.threadContainer}>
+						<a className={styles.thread} href={thread.url} target="_blank" rel="noopener noreferrer">
+							<pre>{JSON.stringify(thread, null, 4)}</pre>
+						</a>
+					</div>
+				))}
+			</StackGrid>
+		</Fragment>
 	);
 }
 
