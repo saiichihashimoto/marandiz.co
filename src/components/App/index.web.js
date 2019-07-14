8383399a2a13d8e18099eb4f5c54800fecf0edf9
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-07-13 21:42:56 -0700

    header and offerings

--- src/components/App/index.web.js
+++ src/components/App/index.web.js
@@ -2,12 +2,16 @@ import React, { Fragment } from 'react';
 import { Helmet } from 'react-helmet';
 
 import Endorsements from '../Endorsements';
+import Header from '../Header';
+import Offerings from '../Offerings';
 import ThreadBlocks from '../ThreadBlocks';
 
 function App() {
 	return (
 		<Fragment>
 			<Helmet defaultTitle="marandiz.co" titleTemplate="%s | marandiz.co" />
+			<Header />
+			<Offerings />
 			<Endorsements />
 			<ThreadBlocks />
 		</Fragment>
