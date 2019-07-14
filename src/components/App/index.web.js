368b9c7c6f46880852df98acaabce76e4008a339
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-07-14 15:34:04 -0700

    react-snap is back, so are some settings

--- src/components/App/index.web.js
+++ src/components/App/index.web.js
@@ -9,7 +9,18 @@ import ThreadBlocks from '../ThreadBlocks';
 function App() {
 	return (
 		<Fragment>
-			<Helmet defaultTitle="marandiz.co" titleTemplate="%s | marandiz.co" />
+			<Helmet defaultTitle="marandiz.co" titleTemplate="%s | marandiz.co">
+				<meta charset="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+				<meta name="theme-color" content="#000000" />
+				<meta property="og:site_name" content="Pinterest Ad Examples" />
+				<link rel="shortcut icon" href={`${process.env.PUBLIC_URL}/favicon.ico`} />
+				<link rel="manifest" href={`${process.env.PUBLIC_URL}/manifest.json`} />
+				<link rel="apple-touch-icon" sizes="180x180" href={`${process.env.PUBLIC_URL}/apple-touch-icon.png`} />
+				<link rel="icon" type="image/png" sizes="32x32" href={`${process.env.PUBLIC_URL}/favicon-32x32.png`} />
+				<link rel="icon" type="image/png" sizes="16x16" href={`${process.env.PUBLIC_URL}/favicon-16x16.png`} />
+				<link rel="manifest" href={`${process.env.PUBLIC_URL}/site.webmanifest`} />
+			</Helmet>
 			<Header />
 			<Offerings />
 			<Endorsements />
