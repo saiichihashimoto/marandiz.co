d8d7e22b2ea9fa8938b9f934e656c3b2b4cd1bf0
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-07-13 20:56:45 -0700

    Endorsements get flexbox

--- src/components/Endorsements/index.web.js
+++ src/components/Endorsements/index.web.js
@@ -6,15 +6,9 @@ import styles from './Endorsements.module.scss';
 function Endorsements() {
 	return (
 		<div className={styles.endorsements}>
-			<div className={styles.endorsementContainer}>
-				<img className={classnames(styles.endorsement, styles.buffer)} alt="Wow, it's buffer" />
-			</div>
-			<div className={styles.endorsementContainer}>
-				<img className={classnames(styles.endorsement, styles.instagram)} alt="Wow, it's instagram" />
-			</div>
-			<div className={styles.endorsementContainer}>
-				<img className={classnames(styles.endorsement, styles.facebook)} alt="Wow, it's facebook" />
-			</div>
+			<img className={classnames(styles.endorsement, styles.buffer)} alt="Wow, it's buffer" />
+			<img className={classnames(styles.endorsement, styles.instagram)} alt="Wow, it's instagram" />
+			<img className={classnames(styles.endorsement, styles.facebook)} alt="Wow, it's facebook" />
 		</div>
 	);
 }
