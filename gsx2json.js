7c46775a918b73ef7af339313e82bae4c9816c58
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-07-28 19:59:20 -0700

    preload images for build

--- gsx2json.js
+++ gsx2json.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const fs = require('fs').promises;
 
 const request = require('request-promise-native');
@@ -8,18 +9,58 @@ async function gsx2json() {
 		json: true,
 	});
 
-	const entries = raw.map(({
+	const rawFixed = raw.map(({
 		id:            { $t: id },
 		gsx$image:     { $t: image },
 		gsx$timestamp: { $t: timestamp },
 		...row
-	}) => [
-		...Object.entries({
-			id:        id.replace(/^.*\//u, ''),
-			image:     image.replace(/^https:\/\/drive\.google\.com\/open\?id=(.*)$/u, 'https://drive.google.com/uc?id=$1'),
-			timestamp: (new Date(timestamp)).valueOf(),
-		}),
-		...Object.entries(row)
+	}) => ({
+		gsx$id:        { $t: id.replace(/^.*\//u, '') },
+		gsx$image:     { $t: image.replace(/^https:\/\/drive\.google\.com\/open\?id=(.*)$/u, 'https://drive.google.com/uc?id=$1') },
+		gsx$timestamp: { $t: (new Date(timestamp)).valueOf() },
+		...row,
+	}));
+
+	const imagesRedirected = process.argv.some((arg) => arg === '--local')
+		? rawFixed
+		: await Promise.all(rawFixed.map(async ({
+			gsx$image: { $t: image } = {},
+			...row
+		}) => {
+			if (!image) {
+				return row;
+			}
+
+			let newImage = image;
+
+			try {
+				const { request: { href } } = await request(image, {
+					resolveWithFullResponse: true,
+				});
+
+				newImage = href;
+			} catch (err) {
+				// eslint-disable-next-line no-console
+				console.warn('Couldn\'t redirect the image, using original', err);
+
+				return {
+					...row,
+					gsx$image: { $t: image },
+				};
+			}
+
+			if (newImage.startsWith('https://accounts.google.com/ServiceLogin')) {
+				throw new Error(`${image} needs it's "Link Sharing" settings set to "Public on the web". Images will not work otherwise!`);
+			}
+
+			return {
+				...row,
+				gsx$image: { $t: newImage },
+			};
+		}));
+
+	const entries = imagesRedirected.map(
+		(row) => Object.entries(row)
 			.map(([key, { $t: value }]) => [key, value])
 			.filter(([key, value]) => key.startsWith('gsx$') && value !== '')
 			.map(([key, value]) => [key.substr(4), value])
@@ -28,8 +69,8 @@ async function gsx2json() {
 				Number.isNaN(Number(value))
 					? value
 					: Number(value),
-			]),
-	]);
+			])
+	);
 
 	const json = {
 		columns2: Object.fromEntries(
@@ -58,4 +99,11 @@ async function gsx2json() {
 	}
 }
 
-gsx2json();
+gsx2json()
+	// eslint-disable-next-line promise/prefer-await-to-callbacks
+	.catch((err) => {
+		// eslint-disable-next-line no-console
+		console.error(err);
+
+		process.exit(1);
+	});
