5efcdd817e70c0b5960e56afa93eb0f6fe58b668
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-10-06 16:28:02 -0700

    deleting first offering

--- src/components/Offerings/index.web.js
+++ src/components/Offerings/index.web.js
@@ -14,30 +14,6 @@ function Offerings() {
 				</h2>
 			</div>
 			<div className={styles.offerings}>
-		{/*<div className={styles.offering}>
-					<h3 className={styles.subHeading}>
-						Unblock Your Company
-					</h3>
-					<h2 className={styles.heading}>
-						Strategy Sessions
-					</h2>
-					<p className={styles.paragraph}>
-						You’ve got problems, and we like problem solving. Whether starting on a new
-						project or scaling existing offerings, this 4-hour meeting aims to generate
-						valuable solutions and insights while taking into account team size and
-						capabilities.
-					</p>
-					<p className={styles.paragraph}>
-						Get confidence that your next steps are thoughtfully inspected and ready for
-						action. We’ll follow up with a short summary and relevant action items.
-					</p>
-					<p className={styles.paragraph}>
-						After signing up, we will reach out within one business day to schedule our
-						conversation.
-					</p>
-					<div className={styles.spacer} />
-					<a className={styles.cta} href={`mailto:${email}?subject=Strategy Session`} target="_blank" rel="noopener noreferrer">Get Started</a>
-				</div>*/}
 				<div className={styles.offering}>
 					<h3 className={styles.subHeading}>
 						DTC Strategy
@@ -46,15 +22,16 @@ function Offerings() {
 						Strategy & Audits
 					</h2>
 					<p className={styles.paragraph}>
-						Coupling our experience with industry specific research, 
-						we identify leverage points for your brand, 
-						product (digital and physical), and marketing, 
-						advising you on the best next steps. 
-						We work with you on a per-project basis, providing data, 
+						Coupling our experience with industry specific research,
+						we identify leverage points for your brand,
+						product (digital and physical), and marketing,
+						advising you on the best next steps.
+						We work with you on a per-project basis, providing data,
 						insights, and advice to drive towards your business objectives.
 					</p>
 					<p className={styles.paragraph}>
-						In 30 days, our partners receive an audit along with actionable insights for future execution. We also meet in-person as needed.
+						In 30 days, our partners receive an audit along with actionable
+						insights for future execution. We also meet in-person as needed.
 					</p>
 					<div className={styles.spacer} />
 					<a className={styles.cta} href={`mailto:${email}?subject=Growth Partnership`} target="_blank" rel="noopener noreferrer">Get in Touch</a>
